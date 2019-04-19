7ca07501ff9ef6b388e88bd41ac6ca41bb288681
ryabkov <gold_100@bk.ru>
2019-04-19 18:54:48 +0300

    main page

---
+++ app/js/app.js
@@ -0,0 +1,124 @@
+'use strict';
+
+$(document).ready(function () {
+	var preloader = $('.preload'),
+			truck = preloader.find('.truck'),
+			road = preloader.find('.road'),
+			timer;
+
+	road.addClass('road-move');
+	
+	road.on('transitionend', function () {
+		truck.addClass('visible');
+		truck.on('transitionend', function () {
+			truck.addClass('move');
+			truck.on('transitionend', function () {
+				truck.removeClass('visible');
+				truck.on('transitionend', function () {
+					road.addClass('full');
+					road.on('transitionend', function () {
+						preloader.addClass('loaded');
+						preloader.on('transitionend', function () {
+							$('body').removeClass('loading');
+							timer = setTimeout(function () {
+								preloader.remove();
+							}, 1000);
+						});
+					});
+				});
+			});
+		});
+	});
+	clearInterval(timer);
+});
+
+$(function () {
+
+	$('ul.uk-switcher img.lazy').lazy({
+		bind: "event"
+	});
+
+	var App = {
+		init: function () {
+			this.navigation.init();
+			this.searchForm.init();
+			this.UIkit.init();
+			console.log('Started');
+		},
+		UIkit: {
+			init: function () {
+				this.dropdown();
+			},
+			dropdown: function () {
+				var dp = UIkit.dropdown($('.dropdown-menu'));
+
+				UIkit.util.on($('.dropdown-menu'),'hide', function () {
+					$('.category-nav li').removeClass('active');
+					$('.category-subnav').removeClass('visible');
+				});
+			}
+		},
+		navigation: {
+			init: function () {
+				this.categoryNavLinksHover();
+			},
+			categoryNavLinksHover: function () {
+
+				var categorySubnavList = $('.category-subnav'), 
+					idx, 
+					parentBlock;
+
+				$('.category-nav').on('mouseenter', 'li', function () {
+
+					idx = $('.category-nav li').index($(this));
+
+					parentBlock = $(this).parents($('div[uk-dropdown]'));
+
+					$(this).addClass('active').siblings('li').removeClass('active');
+
+					categorySubnavList.siblings('div').removeClass('visible');
+
+					parentBlock.find($('.category-subnav').eq(idx)).addClass('visible');
+
+				});
+			}
+		},
+		searchForm: {
+			init: function () {
+				this.showForm();
+			},
+			showForm: function () {
+				var _self = this;
+
+				$('.js__toggle-form').on('click', function (e) {
+					e.preventDefault();
+
+					$('.search-form').addClass('visible');
+
+					$('.search-form').on('transitionend', function () {
+						$(this).find('input[type="text"]').focus();
+					});
+
+					$(document).on('click keyup', _self.hiddenForm);
+
+				});
+			},
+			hiddenForm: function (e) {
+				var form = document.querySelector('.search-form'),
+					input = form.querySelector('input'),
+						btn = form.querySelector('.search-form__btn');
+
+				$(document).on('click keyup', function (e) {
+					if((e.which == 27) || (e.target != input && e.target != btn) && $(form).hasClass('visible')) {
+						$(input).val('');
+						$('.search-form').removeClass('visible');
+						$(document).unbind('click keyup', this.hiddenForm);
+					}
+				});
+			}
+		}
+	};
+
+	App.init();
+
+});
---
+++ app/js/sliders.js
@@ -0,0 +1,74 @@
+$(function () {
+	$('.main-slider .slider').slick({
+		arrows: true,
+		dots: true,
+		infinity: true,
+		cssEase: 'linear',
+		lazyLoad: 'ondemand',
+		autoplay: false,
+		fade: false,
+		autoplaySpeed: 2000,
+		speed: 400,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: $('.main-slider').find('.slide-prev'),
+		nextArrow: $('.main-slider').find('.slide-next'),
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 940,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
+	$('.main-slider .slick-dots').appendTo('.main-slider-arrows-wrapper .uk-container');
+
+	$('.about-company-slider .slider').slick({
+		arrows: true,
+		dots: false,
+		infinity: true,
+		lazyLoad: 'ondemand',
+		autoplay: true,
+		fade: true,
+		autoplaySpeed: 4000,
+		speed: 400,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: $('.about-company-slider').find('.slide-prev'),
+		nextArrow: $('.about-company-slider').find('.slide-next'),
+	});
+
+	$('.offers-slider .slider').slick({
+		arrows: true,
+		dots: false,
+		infinity: true,
+		lazyLoad: 'ondemand',
+		autoplay: true,
+		fade: false,
+		autoplaySpeed: 4000,
+		speed: 400,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		prevArrow: $('.offers-slider ').find('.slide-prev'),
+		nextArrow: $('.offers-slider ').find('.slide-next'),
+	});
+
+});
